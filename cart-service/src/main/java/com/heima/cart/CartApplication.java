a515157622c67bbdd5191038cc82c6e91d0fb1d7
Lenovo <439918391@qq.com>
2025-03-18 16:01:33 +0800

    登录模块

---
+++ cart-service/src/main/java/com/heima/cart/CartApplication.java
@@ -0,0 +1,17 @@
+package com.heima.cart;
+
+import com.heima.api.client.ItemClient;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+//@EnableFeignClients(basePackages = "com.heima.api.client")
+@EnableFeignClients(clients = {ItemClient.class})
+@SpringBootApplication
+@MapperScan("com.heima.cart.mapper")
+public class CartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CartApplication.class, args);
+    }
+}
