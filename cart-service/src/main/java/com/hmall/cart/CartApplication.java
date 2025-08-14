433a252343492d94823c331d5fcb00a03863dfc8
utaoki <439918391@qq.com>
2025-08-14 12:39:26 +0800

    拆分完成

---
+++ cart-service/src/main/java/com/hmall/cart/CartApplication.java
@@ -0,0 +1,27 @@
+package com.hmall.cart;
+
+import com.hmall.api.client.ItemClient;
+import com.hmall.api.config.FeignLogConfig;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@MapperScan("com.hmall.cart.mapper")
+@SpringBootApplication
+@EnableFeignClients(basePackages = "com.hmall.api.client",
+        defaultConfiguration = FeignLogConfig.class)
+//@EnableFeignClients(clients = {ItemClient.class})
+public class CartApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CartApplication.class, args);
+    }
+
+
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
---
+++ pay-service/src/main/java/com/hmall/pay/PayApplication.java
@@ -0,0 +1,20 @@
+package com.hmall.pay;
+
+import com.hmall.api.config.FeignLogConfig;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@MapperScan("com.hmall.pay.mapper")
+@SpringBootApplication
+@EnableFeignClients(basePackages = "com.hmall.api.client",
+        defaultConfiguration = FeignLogConfig.class)
+public class PayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PayApplication.class, args);
+    }
+
+
+
+}
